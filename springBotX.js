fe41d3c8b47e53ed97a4775a87b7d999b1958b5b
malpig <115734177+malpig@users.noreply.github.com>
2023-04-09 15:39:04 -0400

    Add files via upload

--- springBotX.js
+++ springBotX.js
@@ -4,33 +4,44 @@ const context = canvas.getContext("2d");
 context.fillStyle = `rgb(0,0,0)`;
 context.fillRect(0, 0, canvas.width, canvas.height);
 var cLeft = canvas.offsetLeft + canvas.clientLeft, cTop = canvas.offsetTop + canvas.clientTop;
-var startTime=0, currentTime=0, seconds=0;
-// Add event listener for `click` events.
+var startTime = 0, currentTime = 0, seconds = 0;
+var tMax = 530;
+var iid;
+var xpos = [0, 0, 0], xvel = [0, 0, 0], ypos = [0, 0, 0], yvel = [0, 0, 0];
+var g = 60, dt = .03, m = [1, 1, 1];
+var k = [30, 30, 30], eq = [100, 100, 100]
+var floorY = 40;
+var mu = .70;
+var teq;
+var canvasData = context.getImageData(0, 0, canvas.width, canvas.height);
+
 canvas.addEventListener('click', function (event)
 {
     var x = event.pageX - cLeft, y = event.pageY - cTop;
     var i;
     for (i = 0; i < 3; i++)
     {
-        if (x <= 520 && x >= 20)
+        if (x < tMax && x > 20)
         {
-            if (y <= 530 + 100 * i && y >= 500 + 100 * i)
+            if (y < 556 + 112 * i && y > 454 + 112 * i)
             {
-                eq[i] = x - 20;
+                drawRec();
+                teq[i][x - 21] = (555 + 112 * i - y) * 5
                 drawRec();
             }
         }
-            
+    }
+    if (y <= 428 && y >= 422)
+    {
+            tMax = x;
+            if (tMax < 20)
+                tMax = 20;
+            if (tMax > 1181)
+                tMax=1181
+            drawRec();
+        
     }
 }, false);
-var iid;
-var xpos = [0, 0, 0], xvel = [0, 0, 0], ypos = [0, 0, 0], yvel = [0, 0, 0];
-var g = 60, dt = .03, m= [1,1,1];
-var k = [30, 30, 30], eq = [100, 100, 100]
-var floorY = 40;
-var mu = .70;
-
-
 function setColor(cn)
 {
     var col = [0, 0, 0];
@@ -38,32 +49,62 @@ function setColor(cn)
     context.fillStyle = `rgb(${col[0]},${col[1]},${col[2]})`;
     context.strokeStyle = `rgb(${col[0]},${col[1]},${col[2]})`;
 }
-function drawRec()
-{
-    var i;
-    context.fillStyle = `rgb(0,0,0)`;
-    context.fillRect(0,400,canvas.width,400)
-    for (i = 0; i < 3; i++)
-    {
-        setColor(i);
-        context.beginPath();
-        context.moveTo(20, 500 + 100 * i);
-        context.lineTo(520, 500 + 100 * i);
-        context.lineTo(520, 530 + 100 * i);
-        context.lineTo(20, 530 + 100 * i);
-        context.lineTo(20, 500 + 100 * i);
-        context.stroke();
-        context.fillRect(20, 500 + 100 * i, eq[i], 30)
-    }
-}
-function drawcircle(x,y,r)
+function drawcircle(x, y, r)
 {
     if (y > 400)
         y = 395;
     context.beginPath();
-    context.arc(x, 400-y, r, 0, 2 * Math.PI, true);
+    context.arc(x, 400 - y, r, 0, 2 * Math.PI, true);
     context.fill();
- }
+}
+function drawPoint(x, y)
+{
+    var index = (x + y * canvas.width) * 4;
+    canvasData.data[index + 0] = 12;
+    canvasData.data[index + 1] = 76;
+    canvasData.data[index + 2] = 90;
+    canvasData.data[index + 3] = 255;
+    context.putImageData(canvasData, 0, 0);
+}
+function drawRec()
+{
+    var i,j;
+    context.fillStyle = `rgb(0,0,0)`;
+    context.fillRect(0, 400, canvas.width, 400)
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < tMax - 21; j++)
+        {
+            if (teq[i][j] >= 0)
+            {
+                //               context.fillStyle = `rgb(12,76,90)`;
+                drawPoint(j + 21, (555 + 112 * i) - (teq[i][j] / 5));
+                //               context.beginPath();
+                //               context.arc(j + 21, (555 + 112 * i) - (teq[i][j] / 5), 2, 0, 2 * Math.PI, true);
+                //               context.fill();
+            }
+        }
+    }
+    for (i = 0; i < 3; i++)
+    {
+        setColor(i);
+        context.beginPath();
+        context.moveTo(20, 454 + 112 * i);
+        context.lineTo(tMax, 454 + 112 * i);
+        context.lineTo(tMax, 556 + 112 * i);
+        context.lineTo(20, 556 + 112 * i);
+        context.lineTo(20, 454 + 112 * i);
+        context.stroke();
+    }
+    context.strokeStyle = `rgb(0,100,70)`;
+    context.moveTo(20, 425);
+    context.lineTo(1181, 425);
+    context.stroke();
+    context.fillStyle = `rgb(150,50,20)`;
+    context.beginPath();
+    context.arc(tMax, 425, 5, 0, 2 * Math.PI, true);
+    context.fill();
+}
 function evolution()
 {
     var fF = [0,0,0];
@@ -166,6 +207,18 @@ function evolution()
 }
 function go()
 {
+    var i, j;
+    teq = new Array(3);
+    for (i = 0; i < 3; i++)
+      teq[i] = new Array(1160);
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 1160; j++)
+        {
+            teq[i][j] = -1;
+        }
+    }
+
     eq = [100, 100, 100];
     ypos = [floorY + 10, floorY + 10 + eq[0] * .866, floorY + 10];
     xpos = [60, 60 + eq[0] / 2, 60 + eq[0]];
